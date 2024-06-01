2a5d73b8393d4c09d602877c34fba7bf26a201cb
Si1verB1ack <146105684+Si1verB1ack@users.noreply.github.com>
2024-06-02 05:12:08 +0700

    20 jinja

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -6,32 +7,38 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('index.html')
-
-
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
-
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-
-@app.route('/services')
-def services():
-    return render_template('services.html')
-
-
-@app.route('/blog')
-def blog():
-    return render_template('blog.html')
+    return render_template('index.html', current_page='home')
 
 
 @app.route('/shop')
 def shop():
-    return render_template('shop.html')
+    return render_template('shop.html', current_page='shop')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html', current_page='about')
+
+
+@app.route('/services')
+def services():
+    return render_template('services.html', current_page='services')
+
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html', current_page='blog')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html', current_page='contact')
+
+
+@app.route('/jinja')
+def jinja():
+    now = datetime.now()
+    return render_template('jinja.html', now=now)
 
 
 if __name__ == '__main__':
